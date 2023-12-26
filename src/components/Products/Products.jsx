72db7f7d21a1cd73a2f4049b87432d83f625a985
Mich <michxcrz@gmail.com>
2023-12-26 16:47:13 -0300

    integração da api do mercado livre

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import  '../Products/Products.css';
+import fetchProducts from '../../api/fetchProducs';
 
 function Products() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts('iphone').then((response) => {
+      setProducts(response);
+    });
+  }, []);
+
+  console.log(products);
+
   return ( 
     <section className="products container">
       products
