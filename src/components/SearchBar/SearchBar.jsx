8208296f755bf1bcaa4ac3abaed2f03357177547
Mich <michxcrz@gmail.com>
2023-12-26 11:31:07 -0300

    ?

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,11 +4,12 @@ import './SearchBar.css';
 
 function SearchBar() {
 
-  const [searchValue, setSearchValue] = useState('Michelly');
+  const [searchValue, setSearchValue] = useState('');
 
   return ( 
     <form className= "search_bar">
-      <input type="search" placeholder="Buscar produtos" className="search__input" required/>
+      <input type="search" value={searchValue} placeholder="Buscar produtos" className="search__input" onChange={ ({target}) => setSearchValue(target.value) } required 
+      />
       { searchValue }
       <button type="submit"className="search__button">
         < IoIosSearch />
