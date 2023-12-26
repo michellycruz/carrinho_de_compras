3069ee38e23e4642504a9068858d34361649f606
Mich <michxcrz@gmail.com>
2023-12-26 08:51:41 -0300

    search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { IoIosSearch } from 'react-icons/io';
+import './SearchBar.css';
 
 function SearchBar() {
+
+  const [searchValue, setSearchValue] = useState('Michelly');
+
   return ( 
-    <form>
+    <form className= "search_bar">
       <input type="search" placeholder="Buscar produtos" className="search__input" required/>
+      { searchValue }
       <button type="submit"className="search__button">
         < IoIosSearch />
       </button>
