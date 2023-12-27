e8894212d1e30aff92ef295f03fe4a1ed0460fdb
Mich <michxcrz@gmail.com>
2023-12-27 09:15:09 -0300

    css card produto

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import '../ProductCard/ProductCard.css';
+
 function ProductCard() {
   return ( 
     <section className="product__card">
-      product card
+      <img src="https://http2.mlstatic.com/D_608615-MLU72148589298_102023-W.jpg" alt="product" className="card__image" />
+      <div className="card__infos">
+        <h2 className="card__price">R$300.00</h2>
+        <h2 className="card__title">Kindle</h2>
+      </div>
+
+      <button type="button" className="btn__add__card">+</button>
     </section>
   );
 }
