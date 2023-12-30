3a2a27c39db3dbffa038379acffd139a067ea42f
Mich <125368412+michellycruz@users.noreply.github.com>
2023-12-29 23:02:37 -0300

    ?

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Cart/Cart.css';
+
+function Cart() {
+  return ( 
+    <section>
+      <div className="cart-items">Itens do Carrinho</div>
+      <div className="cart-resume">Resumo do Carrinho</div>
+    </section>
+  );
+}
+
+export default Cart;
