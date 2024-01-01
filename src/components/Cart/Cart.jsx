bd77970c87fd7df178b69ccabf0251e363a9fe88
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-01 13:28:38 -0300

    mt foda

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,20 @@ import React, { useContext } from 'react';
 import '../Cart/Cart.css';
 import CartItem from '../CartItem/CartItem';
 import AppContext from '../../context/AppContext';
+import formatCurrency from '../../utils/formatCurrency';
 
 function Cart() {
   const {cartItens} = useContext(AppContext);
+
+  const totalPrice = cartItens.reduce((acc, item) => item.price + acc, 0);
 
   return ( 
     <section className="cart">
       <div className="cart-items">
         { cartItens.map((cartItem) => <CartItem key={cartItem.id} data={cartItem}/>) }
-
-
       </div>
-      <div className="cart-resume">Resumo do Carrinho</div>
+
+      <div className="cart-resume">{(formatCurrency(totalPrice, 'BRL'))}</div>
     </section>
   );
 }
