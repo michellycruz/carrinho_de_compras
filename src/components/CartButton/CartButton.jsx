054682bd30102766b25e7ecb046434a84434eb56
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-01 13:34:26 -0300

    lógica quantidade de itens no carrinho

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { IoCartOutline } from 'react-icons/io5';
 import '../CartButton/CartButton.css';
+import AppContext from '../../context/AppContext';
 
 function CartButton() {
+
+  const { cartItens } = useContext(AppContext);
+
   return(
     <button type="button" className="cart__button">
       <IoCartOutline />
-      <span className="cart__status">1</span>
+      {cartItens.length > 0 && <span className="cart__status">{cartItens.length}</span>}
     </button>
   );
 }
